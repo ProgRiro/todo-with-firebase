eb881be92ff56fffc87c7ba7f76fd467d015ec82
progriro <progriro@gmail.com>
2021-01-14 18:41:02 +0900

    [created] input form by material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { FormControl, TextField } from "@material-ui/core";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
   useEffect(() => {
     // db.collection.onSnapshot で firebase 側の変更を監視する
     // 監視しているため、画面を更新しなくても自動で表示が書き換わる
@@ -15,8 +18,29 @@ const App: React.FC = () => {
     // 監視をするための関数が戻り値で返ってきている
     return () => unSub();
   }, []);
+  const newTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    db.collection("tasks").add({ title: input });
+    setInput("");
+  };
   return (
     <div className="App">
+      <h1>React Todo App with Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+
       {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
       ))}
