e374ddd4ab118753e2a1c24fc904c28de66f62df
progriro <progriro@gmail.com>
2021-01-14 18:59:04 +0900

    [add] editTask() and deleteTask()

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "firebase/app";
 import { ListItem, TextField, Grid } from "@material-ui/core";
 import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
 import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import { db } from "../firebase";
 
 interface Props {
   id: string;
   title: string;
 }
 
-const TaskItem: React.FC<Props> = ({ id, title }) => {
+const TaskItem: React.FC<Props> = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const editTask = () => {
+    // merge: true = titleのみ変更する
+    db.collection("tasks").doc(props.id).set({ title: title }, { merge: true });
+  };
+
+  const deleteTask = () => {
+    db.collection("tasks").doc(props.id).delete();
+  };
+
   return (
-    <div>
-      <ListItem>
-        <h2>{title}</h2>
-      </ListItem>
-    </div>
+    <ListItem>
+      <h2>{props.title}</h2>
+      <Grid container justify="flex-end">
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          label="Edit task"
+          value={title}
+          onChange={(
+            e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+          ) => setTitle(e.target.value)}
+        />
+      </Grid>
+      <button onClick={editTask}>
+        <EditOutlinedIcon />
+      </button>
+      <button onClick={deleteTask}>
+        <DeleteOutlineOutlinedIcon />
+      </button>
+    </ListItem>
   );
 };
 
