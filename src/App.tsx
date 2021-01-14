9c2599cc32e81ed8bf0071f548ae13eabd7ca804
progriro <progriro@gmail.com>
2021-01-14 19:10:51 +0900

    [add] use makeStyles by material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { FormControl, TextField, List } from "@material-ui/core";
 import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import React, { useState, useEffect } from "react";
-import styles from "./App.module.css";
+import styles from "./Styles/App.module.css";
 import TaskItem from "./components/TaskItem";
 import { db } from "./firebase";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
   const [input, setInput] = useState("");
+  const classes = useStyles();
+
   useEffect(() => {
     // db.collection.onSnapshot で firebase 側の変更を監視する
     // 監視しているため、画面を更新しなくても自動で表示が書き換わる
@@ -19,15 +33,19 @@ const App: React.FC = () => {
     // 監視をするための関数が戻り値で返ってきている
     return () => unSub();
   }, []);
+
   const newTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     db.collection("tasks").add({ title: input });
     setInput("");
   };
+
   return (
     <div className={styles.app__root}>
       <h1>React Todo App with Firebase</h1>
+      <br />
       <FormControl>
         <TextField
+          className={classes.field}
           InputLabelProps={{
             shrink: true,
           }}
@@ -42,7 +60,7 @@ const App: React.FC = () => {
         <AddToPhotosIcon />
       </button>
 
-      <List>
+      <List className={classes.list}>
         {tasks.map((task) => (
           <TaskItem key={task.id} id={task.id} title={task.title} />
         ))}
